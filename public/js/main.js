2f81b2224609dbb1409f9fe5d1efebb382efb33f
Alisa <alice.afanasieva@gmail.com>
2025-04-06 22:13:54 +0200

    toggle menu javascript

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,16 @@ import { setupFavorites } from './favorites.js';
 import { setupCategoryFilter } from './filter.js';
 import './welcome.js'; 
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".mobile-menu-toggle");
+  const menu = document.querySelector(".navigation-menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   setupFavorites();
   setupCategoryFilter();
