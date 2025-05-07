a9ec09aabff948fe5e8c8e46ec771d92505c5a7a
Alisa <alice.afanasieva@gmail.com>
2025-05-07 10:31:14 +0200

    body.dark class css

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,10 +1,13 @@
 export function setupThemeChange() {
+  const body = document.querySelector("body");
   const modeToggle = document.querySelector(".btn-theme-change");
+
   
   if (!modeToggle) return;
 
   modeToggle.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark-theme");
+    document.documentElement.classList.toggle("dark");
     modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
   });
 }
