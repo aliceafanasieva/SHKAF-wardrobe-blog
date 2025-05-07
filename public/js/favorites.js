fbc80755cf220d034b8ef828805d4029e8985442
Alisa <alice.afanasieva@gmail.com>
2025-05-07 11:43:18 +0200

    Vervang heart-icons door inline SVGs met dynamic theme color
    - Verwijderd <img> gebruik voor hart-iconen en vervangen door inline <svg><use>
    - SVG-symbols toegevoegd aan layout.liquid (icon-heart-full & icon-heart-empty)
    - Kleur van iconen volgt nu automatisch light/dark theme via currentColor
    - favorites.js aangepast: gebruikt nu .getAttribute('href') i.p.v. .src
    - CSS toegevoegd voor .heart-icon (transition + kleur)

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -4,8 +4,8 @@ export function setupFavorites() {
   buttons.forEach(btn => {
     btn.addEventListener("click", async function () {
       const postId = this.dataset.id;
-      const icon = this.querySelector("img");
-      const isFull = icon.src.includes("heart-full.svg");
+      const icon = this.querySelector("use");
+      const isFull = icon.getAttribute("href").includes("icon-heart-full");
 
       try {
         const url = isFull ? "/favorites/remove" : "/favorites";
@@ -16,9 +16,7 @@ export function setupFavorites() {
         });
 
         if (res.ok) {
-          icon.src = isFull
-            ? "/assets/icons/heart-empty.svg"
-            : "/assets/icons/heart-full.svg";
+          icon.setAttribute("href", isFull ? "#icon-heart-empty" : "#icon-heart-full");
         }
       } catch (err) {
         console.error("Fout bij favoriet toevoegen/verwijderen:", err);
